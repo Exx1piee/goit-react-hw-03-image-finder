b946c2c7dc1142e75425ee56c4825fb4957210e3
Exx1piee <130587927+Exx1piee@users.noreply.github.com>
2023-12-19 16:47:04 +0200

    byb

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,105 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import React, { Component } from 'react';
+import { SearchBar } from './Searchbar/searchbar';
+import { ImageGallery } from './ImageGallery/imagegallery';
+import { fetchImage } from 'FetchApi';
+import { Loader } from './Loader/loader';
+import { Button } from './butt/button';
+import { Modal } from './Modal/modal';
+
+export class App extends Component {
+  state = {
+    inputData: '',
+    items: [],
+    page: 1,
+    totalImages: 0,
+    hits: null,
+    isLoading: false,
+    error: '',
+    isShowMore: false,
+    isShowModal: false,
+    modalImage: '',
+  };
+
+  formSubmit = inputData => {
+    console.log(inputData);
+    if (this.state.inputData !== inputData) {
+      this.setState(
+        { inputData, page: 1, items: [], totalImages: 0 },
+        this.getImages
+      );
+    }
+  };
+
+  componentDidMount() {
+    this.getImages();
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    const { inputData, page } = this.state;
+    if (prevState.inputData !== inputData || prevState.page !== page) {
+      this.getImages();
+    }
+  }
+
+  getImages = async () => {
+    const { inputData, page } = this.state;
+    if (!inputData) {
+      return;
+    }
+    try {
+      this.setState({ isLoading: true, error: '' });
+      const response = await fetchImage(inputData, page);
+      this.setState(prevState => ({
+        items: prevState.items.concat(response.hits),
+      }));
+    } catch (error) {
+      console.log(error);
+      this.setState({ error: error.message });
+    } finally {
+      this.setState({ isLoading: false });
+    }
+  };
+
+  //
+
+  handleClick = () => {
+    this.setState(prev => ({ page: prev.page + 1 }), this.getImages);
+  };
+
+  handleImageClick = imageUrl => {
+    this.setState({ modalImage: imageUrl, isShowModal: true });
+  };
+  handleCloseModal = () => {
+    this.setState({ isShowModal: false, modalImage: '' });
+  };
+
+  render() {
+    const { items, isLoading, error, isShowMore, isShowModal, modalImage } =
+      this.state;
+
+    return (
+      <>
+        <SearchBar onSubmit={this.formSubmit} />
+        <ImageGallery
+          items={this.state.items}
+          onImageClick={this.handleImageClick}
+        />
+
+        {isLoading && <Loader />}
+        {error && <h1>{error}</h1>}
+        {items.length > 0 && !isLoading && (
+          <Button onClick={this.handleClick}>
+            {isShowMore ? 'Hide images' : 'Show more'}
+          </Button>
+        )}
+        {isShowModal && (
+          <Modal
+            isOpenModal={isShowModal}
+            item={modalImage}
+            onCloseModal={this.handleCloseModal}
+          />
+        )}
+      </>
+    );
+  }
+}
---
+++ src/components/Searchbar/searchbar.jsx
@@ -0,0 +1,52 @@
+import React from 'react';
+import { Component } from 'react';
+import css from './searchbar.module.css';
+
+export class SearchBar extends Component {
+  state = {
+    inputData: '',
+  };
+
+  onChangeInput = e => {
+    this.setState({ inputData: e.currentTarget.value.toLowerCase() });
+  };
+
+  handleSubmit = e => {
+    e.preventDefault();
+
+    const { inputData } = this.state;
+
+    if (inputData === '') {
+      alert('Please, fill input');
+      return;
+    }
+
+    this.props.onSubmit(this.state.inputData);
+    console.log(this.state);
+    this.setState({ inputData: '' });
+  };
+
+  render() {
+    return (
+      <header className={css.search}>
+        <form className={css.form} onSubmit={this.handleSubmit}>
+          <button className={css.searchBtn} type="submit">
+            <span>Search</span>
+          </button>
+
+          <input
+            name="inputData"
+            value={this.state.inputData}
+            onChange={this.onChangeInput}
+            type="text"
+            autoComplete="off"
+            autoFocus
+            placeholder="Search images and photos"
+            className={css.input}
+          />
+        </form>
+      </header>
+    );
+  }
+}
+export default SearchBar;
